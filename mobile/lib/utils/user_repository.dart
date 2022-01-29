51d4e608cc11813488d480259b920532b82196a6
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-01-29 11:41:28 +0530

    login signup conditions

--- mobile/lib/utils/user_repository.dart
+++ mobile/lib/utils/user_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:foodie/utils/storage_manager.dart';
 import 'package:http/http.dart' as http;
 
 import 'const.dart';
@@ -11,14 +12,28 @@ class UserRepository {
     return jsonDecode(res.body);
   }
 
+  static Future<List> getProducts(String storeID) async {
+    print(storeID);
+    var res = await http
+        .get(Uri.parse("$baseURL/getProductsfromSingleStore/$storeID"));
+    print(jsonDecode(res.body));
+    return jsonDecode(res.body);
+  }
+
   static Future login(int phoneNo, String password) async {
-    var body = {"mobileNo": phoneNo, "password": password};
+    var body = {"mobileNo": phoneNo.toString(), "password": password};
     var res = await http.post(Uri.parse(baseURL + "/login"), body: body);
     var login = jsonDecode(res.body);
     print(login);
     Fluttertoast.showToast(msg: login['message']);
-    if (login != null) {
+    if (login != null && login['data'] != null && login['token'] != null) {
+      StorageManager.saveData('token', login['token']);
+      StorageManager.saveData('id', login['data']['_id']);
+      StorageManager.saveData('name', login['data']['name']);
+      StorageManager.saveData('regNo', login['data']['regNo']);
+      StorageManager.saveData('role', login['data']['role']);
       Fluttertoast.showToast(msg: login['message']);
+      return login;
     } else {
       Fluttertoast.showToast(msg: "Somthing went wrong");
     }
@@ -36,7 +51,7 @@ class UserRepository {
       String dept) async {
     var body = {
       "name": name,
-      "mobileNo": mobileNo,
+      "mobileNo": mobileNo.toString(),
       "password": password,
       "email": email,
       "dateOfBirth": dob,
